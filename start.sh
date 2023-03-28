c7dfa38ea95786be23c588ae52e0829a1717a21b
A.P. Cammeraat <cam@emmauscollege.nl>
2023-03-28 13:32:57 +0000

    sqlite output config file

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
-echo installing node.js packages according to package.json file
-echo this may take some minutes the first time
+# set sqlite output to include headers and colums
+# by create a config file in home folder
+echo "1. creating proper sqlite output config"
+cat << EOF > ~/.sqliterc
+.headers on
+.mode column
+EOF
+
+# install node.js packages
+echo "2. installing node.js packages according to package.json file"
+echo "   this may take some minutes the first time"
 
 yarn install
 # CAM: Dit sloopt de replit compatibliteit
